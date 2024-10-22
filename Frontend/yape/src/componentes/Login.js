a804b2911ddab239f1b8e6b1b9c5d0a4ebd7ea79
Ml1302 <miguel.es.ri1302@gmail.com>
2024-10-21 22:10:23 -0500

    Frontend login

---
+++ Frontend/yape/src/componentes/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import '../stylesheets/Login.css'; // Importa el archivo de estilos
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para autenticar al usuario
+    // Por ejemplo, puedes hacer una solicitud a un servidor para verificar las credenciales
+    if (username === 'admin' && password === 'password') {
+      // Si las credenciales son correctas, redirige al usuario a la página principal
+      window.location.href = '/';
+    } else {
+      setError('Credenciales incorrectas');
+    }
+  };
+
+  return (
+    <div className="split-screen">
+      <div className="left-side">
+        <div className="imagenes">
+              <img alt="Logo yape" height="250" src="https://www.yape.com.pe/assets/images/logo.png"/>
+          </div>
+      </div>
+      <div className="right-side">
+        <div className="login-container">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Usuario:
+              <br />
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>
+              Contraseña:
+              <br />
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <br />
+            <button type="submit">Iniciar sesión</button>
+            {error && <p className="error">{error}</p>}
+            <br />
+            <a href="/register">¿Olvidaste tu contraseña?</a>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
