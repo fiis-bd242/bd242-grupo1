f3a764126f35672a14a930adc46d909f3f54fea7
Ml1302 <miguel.es.ri1302@gmail.com>
2024-10-25 16:05:57 -0500

    Pruebas

---
+++ Frontend/yape/src/componentes/MainMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainMenu() {
+  return (
+    <div>
+      <h2>Menú Principal</h2>
+      {/* Opciones y contenido del menú */}
+    </div>
+  );
+}
+
+export default MainMenu;
