cca98c3cc31580395c9d442549665f48f6e0fa06
Ml1302 <miguel.es.ri1302@gmail.com>
2024-11-08 01:25:40 -0500

    Funcionalidad de correos implementada. Falta solucionar bug de duplicación de observaciones

--- Backend/yape-back/src/main/java/com/example/yapeback/interfaces/EntrevistaRepository.java
+++ Backend/yape-back/src/main/java/com/example/yapeback/interfaces/EntrevistaRepository.java
@@ -1,44 +1,11 @@
 package com.example.yapeback.interfaces;
 
 import com.example.yapeback.model.Entrevista;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-import org.springframework.dao.EmptyResultDataAccessException;
-
 import java.util.Optional;
 
-@Repository
-public class EntrevistaRepository {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    public Entrevista save(Entrevista entrevista) {
-        String sql = "INSERT INTO entrevista (estado, fecha, puntaje_general, id_postulante, id_empleado, tipo_entrevista) VALUES (?, ?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sql, entrevista.getEstado(), entrevista.getFecha(), entrevista.getPuntaje_general(), entrevista.getId_postulante(), entrevista.getId_empleado(), entrevista.getTipo_entrevista());
-        return entrevista;
-    }
-
-    public Entrevista update(Entrevista entrevista) {
-        String sql = "UPDATE entrevista SET estado = ?, fecha = ?, puntaje_general = ?, id_postulante = ?, id_empleado = ?, tipo_entrevista = ? WHERE id_entrevista = ?";
-        jdbcTemplate.update(sql, entrevista.getEstado(), entrevista.getFecha(), entrevista.getPuntaje_general(), entrevista.getId_postulante(), entrevista.getId_empleado(), entrevista.getTipo_entrevista(), entrevista.getId_entrevista());
-        return entrevista;
-    }
-
-    public Optional<Entrevista> findById(Long id) {
-        String sql = "SELECT * FROM entrevista WHERE id_entrevista = ?";
-        try {
-            Entrevista entrevista = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Entrevista.class), id);
-            return Optional.ofNullable(entrevista);
-        } catch (EmptyResultDataAccessException e) {
-            return Optional.empty();
-        }
-    }
-
-    public void deleteById(Long id) {
-        String sql = "DELETE FROM entrevista WHERE id_entrevista = ?";
-        jdbcTemplate.update(sql, id);
-    }
+public interface EntrevistaRepository {
+    Entrevista save(Entrevista entrevista);
+    Entrevista update(Entrevista entrevista);
+    Optional<Entrevista> findById(Long id);
+    void deleteById(Long id);
 }
