ac30037a84bd6cd46786294a2485a2517277e05d
Melannie Muñoz <melannie.munoz.c@uni.pe>
2024-11-27 17:27:04 -0500

    limpiando

--- frontend-gestion-promos/src/components/CrearPromocion.jsx
+++ frontend-gestion-promos/src/components/CrearPromocion.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./CrearPromocion.css";
 
 function CrearPromocion() {
   const navigate = useNavigate();
--- frontend-gestion-promos/src/components/PromocionesActivas.jsx
+++ frontend-gestion-promos/src/components/PromocionesActivas.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import "./PromocionesActivas.css"
 
 const PromocionesActivas = () => {
   const [promociones, setPromociones] = useState([]);
